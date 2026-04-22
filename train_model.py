01cf99a5814aec4a305edc4bdf974bc70f0e63f3
Varun-Kumar-R <152820616+Varun-Kumar-R@users.noreply.github.com>
2026-04-21 22:15:45 -0500

    Refactor train_model.py to improve structure

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import itertools
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, precision_score, f1_score, recall_score
 sns.set(style='white')
